7920477b5126e07344c781c144c24cf010962789
bonchevski <victor.bonchevski@gmail.com>
2018-06-11 23:46:35 +0300

    Done for now

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-console.log('starting app');
+console.log("starting app");
 
-const fs  = require('fs');
-const _ = require('lodash');
+const fs = require("fs");
+const _ = require("lodash");
 
-const notes = require('./notes.js');
+const notes = require("./notes.js");
 console.log(process.argv);
+
+var command = process.argv[2];
+
+console.log("Command :", command);
+
+if (command === "add") {
+  console.log("Adding new note");
+} else if (command === "list") {
+  console.log("listing all notes");
+} else if (command === "read") {
+  console.log("reading note");
+} else if (command === "remove") {
+  console.log("Removing Note");
+} else {
+  console.log("Not recognized");
+}
