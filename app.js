4ff42052352419daa50c6034758a4b529e214a21
bonchevski <victor.bonchevski@gmail.com>
2018-06-11 23:45:25 +0300

    Done with lecture 13  - 11.06.2018 23 : 45 GMT +2

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 console.log('starting app');
 
 const fs  = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-
-console.log('Result: ',  notes.addFunction(9, -2));
-
-// var user = os.userInfo();
-// fs.appendFileSync('greetings.txt', `Hello ${user.username} ! you are ${notes.age} `, function (err){
-//     if(err){
-//         console.log('Error');
-//     }
-// });
+console.log(process.argv);
